262fb6015b3e542c6c86a1c354a647aa2448aa6b
Archit Yadav <127927443+ArchitYad@users.noreply.github.com>
2025-09-30 12:00:56 +0530

    Update home.py with syntax ['page']

--- home.py
+++ home.py
@@ -34,7 +34,7 @@ if uploaded_file is not None:
 
         # Proceed to Analysis button
         if st.button("➡️ Go to Analysis"):
-            st.set_query_params(page="analysis")  # Optional for page navigation
+            st.query_params['page']='analysis'  # Optional for page navigation
             st.experimental_rerun()
 
     except Exception as e:
